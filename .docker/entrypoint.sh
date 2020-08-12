3386ff7f18b8eef06e441e0f8516078f54229dbd
Lucas Massaharu Hirao <lucas.hirao@fatec.sp.gov.br>
2020-08-12 19:41:48 -0300

    mais um teste

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-dockerize -wait tcp://db:3306 -timeout 40s
+dockerize -template ./.docker/app/.env:.env -wait tcp://db:3306 -timeout 40s
+composer install
+php artisan key:generate
+php artisan migrate
 php-fpm
